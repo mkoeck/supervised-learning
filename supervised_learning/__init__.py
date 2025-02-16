d2bf2fe106cb013264bf56ef7c2931d860dc7160
Michael Köck <mkoeck@elektroshopkoeck.com>
2025-02-16 14:55:56 +0100

    [FIX] load demo data from project task not helpdesk ticket

--- supervised_learning/__init__.py
+++ supervised_learning/__init__.py
@@ -4,4 +4,4 @@ from . import models
 
 def _post_load_demo_data(env):
     if env.ref('base.module_supervised_learning').demo:
-        env['helpdesk.ticket']._post_load_demo_data()
+        env['project.task']._post_load_demo_data()
